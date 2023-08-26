d202107abf700cf3ec41a12730515f44c013f1e7
Sergey Malyarov <sergey@malyaroff.com>
2023-08-26 13:41:54 +0300

    create app

---
+++ myproject/catalog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    date_submitted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
