ffd97f61fe7820632dced2f8993956c4e46f2374
Sergey Malyarov <sergey@malyaroff.com>
2023-08-31 11:42:47 +0300

    fixtures

--- myproject/catalog/management/commands/populate_db.py
+++ myproject/catalog/management/commands/populate_db.py
@@ -1,8 +1,11 @@
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 
 class Command(BaseCommand):
-    help = 'Populate the database with initial data'
+    help = 'Populate the database with fixtures'
 
     def handle(self, *args, **kwargs):
-        # Здесь ваш код для заполнения базы данных
+        # вызываем команду `loaddata` для загрузки фикстур.
+        # данные загружаю из файла фикстур `categories.json` в каталоге `fixtures`
+        call_command('loaddata', 'catalog/fixtures/categories.json')
         self.stdout.write(self.style.SUCCESS('Database populated successfully!'))
